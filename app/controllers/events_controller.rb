5403b3dc90fa8b59ac2dfa1027039922c00b4cb2
Tiago Pinto <tpinto@webreakstuff.com>
2008-07-03 19:19:20 +0100

    icon

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,14 +7,14 @@ class EventsController < ApplicationController
   end
   
   def feed
-    @items = []
-    @items << Post.find(:all, :limit => 20)
-    @items << Talk.find(:all, :limit => 20)
-    @items << Comment.find(:all, :limit => 20)
-    
-    @items.flatten!
-    @items.sort!{|a,b| a.created_at <=> b.created_at}
-    @items.slice!(0,20)
+    it = []
+    it << Post.find(:all, :limit => 20)
+    it << Talk.find(:all, :limit => 20)
+    it << Comment.find(:all, :limit => 20)
+      
+    it.flatten!
+    it.sort!{|a,b| a.created_at <=> b.created_at}
+    @items = it.slice(0,20)
     
     response.headers['Content-Type'] = 'application/rss+xml'
     render :layout => false
